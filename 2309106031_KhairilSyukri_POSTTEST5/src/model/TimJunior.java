c1a5b9a72d3c0f2571d7e475dad6a7d6a09ff2c4
khairill <145867760+khairiil@users.noreply.github.com>
2025-04-22 19:10:07 +0800

    Add files via upload

---
+++ 2309106031_KhairilSyukri_POSTTEST5/src/model/TimJunior.java
@@ -0,0 +1,26 @@
+package model;
+
+public class TimJunior extends TimFutsal {
+    private int rataRataUsia;
+
+    public TimJunior(String namaTim, String kotaAsal, int jumlahPemain, int rataRataUsia) {
+        super(namaTim, kotaAsal, jumlahPemain);
+        this.rataRataUsia = rataRataUsia;
+    }
+
+    public int getRataRataUsia() {
+        return rataRataUsia;
+    }
+
+    public void setRataRataUsia(int rataRataUsia) {
+        this.rataRataUsia = rataRataUsia;
+    }
+
+    @Override
+    public void tampilkanInfo() {
+        super.tampilkanInfoDasar();
+        System.out.println("Kategori: Junior");
+        System.out.println("Rata-rata Usia: " + rataRataUsia + " tahun");
+        System.out.println("-----------------------------");
+    }
+}
---
+++ 2309106031_KhairilSyukri_POSTTEST5/src/model/TimSenior.java
@@ -0,0 +1,26 @@
+package model;
+
+public final class TimSenior extends TimFutsal {
+    private int rataRataUsia;
+
+    public TimSenior(String namaTim, String kotaAsal, int jumlahPemain, int rataRataUsia) {
+        super(namaTim, kotaAsal, jumlahPemain);
+        this.rataRataUsia = rataRataUsia;
+    }
+
+    public int getRataRataUsia() {
+        return rataRataUsia;
+    }
+
+    public void setRataRataUsia(int rataRataUsia) {
+        this.rataRataUsia = rataRataUsia;
+    }
+
+    @Override
+    public void tampilkanInfo() {
+        super.tampilkanInfoDasar();
+        System.out.println("Kategori: Senior");
+        System.out.println("Rata-rata Usia: " + rataRataUsia + " tahun");
+        System.out.println("-----------------------------");
+    }
+}
